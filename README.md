b3f52b831fb733b9a42dcbf1e5b096c0a36f1681
sondli <sondre.lillelien@gmail.com>
2024-10-24 15:42:47 +0300

    Added more addons and a simplified list for all addons. Made superwow optional and updated to latest version

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# TWOW-Setup
-![GitHub Release](https://img.shields.io/github/v/release/TheLinuxITGuy/TWOW-Setup?style=for-the-badge&labelColor=%231A365D&color=%23E9FC12)
-![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/TheLinuxITGuy/TWOW-Setup/total?style=for-the-badge&labelColor=%231A365D&color=%23E9FC12)
-
+# twow-setup
+![GitHub Downloads (all assets, all releases)](https://img.shields.io/github/downloads/sondli/twow-setup/total?style=for-the-badge&labelColor=%231A365D&color=%23E9FC12)
 
 ## 🛠️ Installation
-This script makes it easy to install tDF, pfQuest, pfQuest-turtle, SuperAPI, and SuperWoW on Linux
+This script makes it easy to install your preferred addons and SuperWoW on Linux
 
 1. Install twow from Lutris
 2. Download this script
-3. From a Terminal: 
+3. Edit the script to include the addons you want
+4. From a Terminal: 
     1. `chmod u+x TWOW-Setup.sh`
-    2. Type: `./TWOW-Setup.sh`
-    3. Point Lutris to `SuperWoWlauncher.exe`
-    4. Start Turtle WoW and enjoy
-
-## 💖 Donate
-https://www.paypal.com/donate/?hosted_button_id=WPTX2BMBARSG2
+    2. Type: `./setup.sh`
+5. Point Lutris to `SuperWoWlauncher.exe`
+6. Start Turtle WoW and enjoy
---
+++ setup.sh
@@ -0,0 +1,61 @@
+# Inspired by TheLinuxITGuy https://github.com/TheLinuxITGuy/TWOW-Setup/
+
+# Define TWOW and addons directories
+TWOW_DIR="/home/$USER/Games/turtlewow/drive_c/Program Files (x86)/TurtleWoW"
+ADDONS_DIR="$TWOW_DIR/Interface/AddOns"
+
+# Navigate to the AddOns directory
+cd "$ADDONS_DIR"
+
+# List all addons to install
+declare -a ADDONS=(
+"shagu/pfQuest"
+"shagu/pfQuest-turtle"
+"shagu/pfUI"
+"shagu/pfUI-eliteoverlay"
+"doorknob6/pfUI-turtle"
+"balakethelock/SP_SwingTimer"
+"balakethelock/SuperAPI"
+"MarcelineVQ/Roid-Macros"
+"MarcelineVQ/TankPlates"
+"Lexiebean/AtlasLoot"
+"pepopo978/BetterCharacterStats"
+"tdymel/DPSMate"
+"laytya/KLHThreatMeter"
+"gwetchen/aux-addon-turtle"
+)
+
+#Install all addons
+for ADDON in "${ADDONS[@]}"
+do 
+	FOLDER=`cut -d "/" -f2- <<< "$ADDON"`
+
+	if [ "$FOLDER" = "aux-addon-turtle" ] && [ -d "./aux-addon" ]; then
+		echo "Already installed: aux-addon-turtle"
+		continue
+	fi
+
+	if [ -d "./$FOLDER" ]; then
+		echo "Already installed: ${FOLDER}"
+	else
+		git clone "https://github.com/${ADDON}.git"
+		if [ -d "./aux-addon-turtle" ]; then
+			mv ./aux-addon-turtle ./aux-addon
+		fi
+	fi
+done
+
+# Install SuperWoW
+SUPERWOW_VERSION="1.1.3"
+echo "Do you wish to install SuperWoW ${SUPERWOW_VERSION}?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+					cd "$TWOW_DIR"
+					wget https://github.com/balakethelock/SuperWoW/releases/download/Release/SuperWoW.release.${SUPERWOW_VERSION}.zip
+					unzip -o SuperWoW.release.${SUPERWOW_VERSION}.zip
+					rm SuperWoW.release.${SUPERWOW_VERSION}.zip; break;;
+        No ) exit;;
+    esac
+done
+
